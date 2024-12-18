2371505aad2e7e385cf5472d15d1ab15a0883c8f
jottiana <11.paradise.avenue@gmail.com>
2024-12-18 17:29:39 +0100

    fetched data from API

--- client/src/pages/Comparer/Comparer.tsx
+++ client/src/pages/Comparer/Comparer.tsx
@@ -1,31 +1,53 @@
 import "./Comparer.css";
+import { useEffect, useState } from "react";
 import VehicleCard from "../../components/VehicleCard/VehicleCard";
 import type { Vehicle } from "../../types/types";
 
 function Comparer() {
-  const vehicle: Vehicle = {
-    id: 1,
-    brand: "Volskwagen",
-    model: "Golf VII",
-    imageUrl:
-      "https://www.autoscout24.fr/cms-content-assets/67HYXLkjCsA7ukfTtleZ8V-55834a528d1538b246ce20f6f255f847-vw-golf-7-l-01-1100.jpg",
-    licensePlate: "AB-123-CD",
-    registrationDate: "2013-01-01",
-    price: 30000,
-    carbonFootprint: 116,
-    critAirCard: 1,
-    horsepower: 130,
-    powerType: "essence",
-    consumption: 5,
-    drivenDistance: 1000,
-    fuelCost: 90,
-  };
+  const [vehicleId, setVehicleId] = useState<number>(1);
+  const [vehicle, setVehicle] = useState<Vehicle | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchVehicle = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://api-yooliz-lhexs1fkj-paradises-projects-11b7cb6c.vercel.app/api/vehicles/${vehicleId}`,
+        );
+
+        if (!response.ok) {
+          throw new Error("Erreur lors de la récupération du véhicule");
+        }
+
+        const data: Vehicle = await response.json();
+        setVehicle(data);
+      } catch (err) {
+        console.error(err);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchVehicle();
+  }, [vehicleId]);
+
+  if (loading) return <p>Chargement...</p>;
+  if (error) return <p>Erreur lors du chargement du véhicule.</p>;
 
   return (
     <div>
       <h1>Comparer les véhicules</h1>
+      <input
+        type="number"
+        value={vehicleId}
+        onChange={(e) => setVehicleId(Number(e.target.value))}
+        placeholder="Entrez l'ID du véhicule"
+      />
       <div className="vehicle-list">
-        <VehicleCard key={vehicle.id} vehicle={vehicle} />
+        {vehicle && <VehicleCard key={vehicle.id} vehicle={vehicle} />}
       </div>
     </div>
   );
